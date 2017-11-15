b3801f92bd6bceb515cb96bf099c17f57fdc5445
Austin Mielke <amielkedev@gmail.com>
2017-11-15 14:39:37 -0600

    Added links to image slider on home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -26,21 +26,21 @@
                         <div class="carousel-item carousel-item1 active">
                             <img class="img-fluid" src="img/carousel/1.jpg" alt="First slide">
                             <div class="carousel-caption">
-                                <h3><a href="#">Midsummer Night Shawl</a></h3>
+                                <h3><a href="/patterns/503942">Midsummer Night Shawl</a></h3>
                                 <p class="d-none d-sm-block">A timeless, yet versitile lace wrap.</p>
                             </div>
                         </div>
                         <div class="carousel-item">
                             <img class="img-fluid" src="img/carousel/2.jpg" alt="Second slide">
                             <div class="carousel-caption">
-                                <h3><a href="#">Phoenix Rising</a></h3>
+                                <h3><a href="/patterns/546397">Phoenix Rising</a></h3>
                                 <p class="d-none d-sm-block">The dramatic, iconic phoenix has been the symbol of strength and majesty since the dawn of time</p>
                             </div>
                         </div>
                         <div class="carousel-item">
                             <img class="img-fluid" src="img/carousel/3.jpg" alt="Third slide">
                             <div class="carousel-caption">
-                                <h3><a href="#">Buffalo Round Up DK</a></h3>
+                                <h3><a href="patterns/630256">Buffalo Round Up DK</a></h3>
                                 <p class="d-none d-sm-block">Glamorous on all body types, done in a versatile dk weight.</p>
                             </div>
                         </div>
